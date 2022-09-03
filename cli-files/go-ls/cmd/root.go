792a42f14074891fd0d4378ac38abee82a1bf48e
Berkeli Halmyradov <berkelih@gmail.com>
2022-09-03 11:16:52 +0100

    go-ls complete with testing and extra tasks

--- cli-files/go-ls/cmd/root.go
+++ cli-files/go-ls/cmd/root.go
@@ -3,12 +3,12 @@ package cmd
 
 import (
 	"fmt"
-	"io/ioutil"
-	"log"
 	"os"
 
 	"github.com/spf13/cobra"
 )
+
+var Mode bool
 
 func Execute() {
 	if err := golsCmd.Execute(); err != nil {
@@ -21,21 +21,53 @@ var golsCmd = &cobra.Command{
 	Use:   "go-ls",
 	Short: "go-ls is an alternative to ls, allows you to list files in a given directory",
 	Long:  "go-ls is an alternative to ls, allows you to list files in a given directory. By default it lists files in the current working directory. You can also specify a directory to list files in.",
-	RunE: func(cmd *cobra.Command, args []string) error {
-		path, err := os.Getwd()
+	RunE:  GolsCmdFunc,
+}
+
+func init() {
+	golsCmd.PersistentFlags().BoolVarP(&Mode, "mode", "m", false, "Selects the mode of operation")
+}
+
+func GolsCmdFunc(cmd *cobra.Command, args []string) error {
+
+	var path string
+	var err error
+
+	if len(args) > 0 {
+		path = args[0]
+	} else {
+		path, err = os.Getwd()
 		if err != nil {
-			log.Println(err)
+			return err
 		}
+	}
 
-		files, err := ioutil.ReadDir(path)
-		if err != nil {
-			log.Fatal(err)
-		}
+	argStat, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
 
-		for _, file := range files {
-			fmt.Println(file.Name())
-		}
-
+	if !argStat.IsDir() {
+		fmt.Println(path)
 		return nil
-	},
+	}
+
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return err
+	}
+
+	for i, file := range files {
+		out := file.Name()
+
+		if Mode && i < len(files)-1 {
+			out += ", "
+		} else {
+			out += "\n"
+		}
+
+		fmt.Printf(out)
+	}
+
+	return nil
 }
---
+++ cli-files/go-ls/cmd/root_test.go
@@ -0,0 +1,90 @@
+package cmd
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestGolsCmdFunc(t *testing.T) {
+	t.Run("No path provided", func(t *testing.T) {
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		cmd := golsCmd
+		cmd.Execute()
+
+		w.Close()
+		want := "root.go\nroot_test.go\n"
+
+		got, _ := ioutil.ReadAll(r)
+
+		AssertStringsEqual(t, string(got), want)
+	})
+
+	t.Run("Relative Path provided", func(t *testing.T) {
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		cmd := golsCmd
+		cmd.SetArgs([]string{"../assets"})
+		cmd.Execute()
+
+		w.Close()
+		want := "dew.txt\nfor_you.txt\nrain.txt\n"
+
+		got, _ := ioutil.ReadAll(r)
+
+		AssertStringsEqual(t, string(got), want)
+	})
+
+	t.Run("File name provided", func(t *testing.T) {
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		cmd := golsCmd
+		cmd.SetArgs([]string{"../assets/dew.txt"})
+		cmd.Execute()
+
+		w.Close()
+		want := "../assets/dew.txt\n"
+
+		got, _ := ioutil.ReadAll(r)
+
+		AssertStringsEqual(t, string(got), want)
+	})
+
+	t.Run("Invalid path provided", func(t *testing.T) {
+
+		cmd := golsCmd
+		cmd.SetArgs([]string{"../assets/invalid"})
+		err := cmd.Execute()
+
+		if err == nil {
+			t.Errorf("expected an error but didn't get one")
+		}
+	})
+
+	t.Run("out put with -m flag", func(t *testing.T) {
+		r, w, _ := os.Pipe()
+		os.Stdout = w
+
+		cmd := golsCmd
+		cmd.SetArgs([]string{"-m", "../assets"})
+		cmd.Execute()
+
+		w.Close()
+		want := "dew.txt, for_you.txt, rain.txt\n"
+
+		got, _ := ioutil.ReadAll(r)
+
+		AssertStringsEqual(t, string(got), want)
+	})
+}
+
+func AssertStringsEqual(t *testing.T, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
