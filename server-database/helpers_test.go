7f9cf7604532f800bc5261e6ab784784066ffb96
Berkeli Halmyradov <berkelih@gmail.com>
2022-09-25 17:48:41 +0100

    Update to use testify package everywhere

--- server-database/helpers_test.go
+++ server-database/helpers_test.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"errors"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestConnectToDB(t *testing.T) {
@@ -19,33 +22,19 @@ func TestConnectToDB(t *testing.T) {
 func TestValidateIndent(t *testing.T) {
 	t.Run("Valid indent", func(t *testing.T) {
 		i, err := ValidateIndent("2")
-		if err != nil {
-			t.Errorf("Expected indent to be valid, received %v", err)
-		}
-		if i != 2 {
-			t.Errorf("Expected indent to be 2, received %v", i)
-		}
+		require.NoError(t, err)
+		require.Equal(t, 2, i)
 	})
 
 	t.Run("Negative indent", func(t *testing.T) {
 		_, err := ValidateIndent("-1")
-		if err == nil {
-			t.Errorf("Expected indent to be invalid, received %v", err)
-		}
-
-		if err.Error() != "Indent cannot be negative: -1" {
-			t.Errorf("Expected: Indent cannot be negative: -1, received %v", err.Error())
-		}
+		require.Error(t, err)
+		require.ErrorIs(t, err, errors.New("Indent cannot be negative: -1"))
 	})
 
 	t.Run("Invalid indent", func(t *testing.T) {
 		_, err := ValidateIndent("a")
-		if err == nil {
-			t.Errorf("Expected indent to be invalid, received %v", err)
-		}
-
-		if err.Error() != "Unable to parse indent: a" {
-			t.Errorf("Expected: Unable to parse indent: a, received %v", err.Error())
-		}
+		require.Error(t, err)
+		require.ErrorIs(t, err, errors.New("Unable to parse indent: a"))
 	})
 }
