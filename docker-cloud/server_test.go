36269f664732cc1332238550fa5dbba06e621d0d
Berkeli Halmyradov <berkelih@gmail.com>
2022-10-03 21:39:49 +0100

    refactor tests to use a test table

--- docker-cloud/server_test.go
+++ docker-cloud/server_test.go
@@ -10,6 +10,20 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+var TestTable = map[string]struct {
+	endpoint string
+	expected string
+}{
+	"Index should return hello world": {
+		endpoint: "/",
+		expected: "Hello, world!",
+	},
+	"Ping should return pong": {
+		endpoint: "/ping",
+		expected: "Pong!",
+	},
+}
+
 func TestServer(t *testing.T) {
 
 	pool, err := dockertest.NewPool("")
@@ -22,53 +36,31 @@ func TestServer(t *testing.T) {
 		require.NoError(t, pool.Purge(resource), "failed to remove container")
 	})
 
-	t.Run("Index should return hello world", func(t *testing.T) {
+	for name, tt := range TestTable {
+		t.Run(name, func(t *testing.T) {
 
-		var resp *http.Response
+			var resp *http.Response
 
-		err = pool.Retry(func() error {
-			resp, err = http.Get(fmt.Sprint("http://localhost:", resource.GetPort("80/tcp"), "/"))
-			if err != nil {
-				t.Log("container not ready, waiting...")
-				return err
-			}
-			return nil
+			err = pool.Retry(func() error {
+				resp, err = http.Get(fmt.Sprintf("http://localhost:%s%s", resource.GetPort("80/tcp"), tt.endpoint))
+				if err != nil {
+					t.Log("container not ready, waiting...")
+					return err
+				}
+				return nil
+			})
+
+			require.NoError(t, err)
+
+			defer resp.Body.Close()
+
+			require.Equal(t, http.StatusOK, resp.StatusCode)
+
+			body, err := io.ReadAll(resp.Body)
+			require.NoError(t, err)
+
+			require.Equal(t, tt.expected, string(body))
 		})
+	}
 
-		require.NoError(t, err)
-
-		defer resp.Body.Close()
-
-		require.Equal(t, http.StatusOK, resp.StatusCode)
-
-		body, err := io.ReadAll(resp.Body)
-		require.NoError(t, err)
-
-		require.Equal(t, "Hello, world!", string(body))
-	})
-
-	t.Run("Ping should return pong", func(t *testing.T) {
-
-		var resp *http.Response
-
-		err = pool.Retry(func() error {
-			resp, err = http.Get(fmt.Sprint("http://localhost:", resource.GetPort("80/tcp"), "/ping"))
-			if err != nil {
-				t.Log("container not ready, waiting...")
-				return err
-			}
-			return nil
-		})
-
-		require.NoError(t, err)
-
-		defer resp.Body.Close()
-
-		require.Equal(t, http.StatusOK, resp.StatusCode)
-
-		body, err := io.ReadAll(resp.Body)
-		require.NoError(t, err)
-
-		require.Equal(t, "Pong!", string(body))
-	})
 }
