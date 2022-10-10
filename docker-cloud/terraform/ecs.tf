378c888021490ecedfec434556d99188eff92362
Berkeli Halmyradov <berkelih@gmail.com>
2022-10-10 20:45:08 +0100

    gh action to apply terraform changes

--- docker-cloud/terraform/ecs.tf
+++ docker-cloud/terraform/ecs.tf
@@ -4,29 +4,34 @@ resource "aws_ecs_cluster" "docker_cloud" {
 
 resource "aws_ecs_task_definition" "docker_cloud" {
   family                   = "docker-cloud"
-  network_mode             = "host"
-  requires_compatibilities = ["EC2"]
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
   container_definitions = jsonencode(
     [{
       name : "docker-cloud",
-      image : "${aws_ecrpublic_repository.docker_cloud.repository_uri}:latest",
+      image : "${aws_ecrpublic_repository.docker_cloud.repository_uri}:${var.image_tag}",
+      essential : true,
       portMappings : [
         {
-          containerPort : 8080,
-          hostPort : 8080,
+          containerPort : 80,
+          hostPort : 80,
           protocol : "tcp"
         },
       ]
   }])
 
 }
-
 resource "aws_ecs_service" "docker_cloud" {
-  name            = "docker-cloud"
-  cluster         = aws_ecs_cluster.docker_cloud.id
-  task_definition = aws_ecs_task_definition.docker_cloud.arn
-  desired_count   = 1
-  launch_type     = "EC2"
+  name                = "docker-cloud"
+  cluster             = aws_ecs_cluster.docker_cloud.id
+  task_definition     = aws_ecs_task_definition.docker_cloud.arn
+  scheduling_strategy = "REPLICA"
+  launch_type         = "FARGATE"
+  desired_count       = 1
+
+  network_configuration {
+    subnets = data.aws_subnets.ids
+  }
 }
---
+++ docker-cloud/terraform/variables.tf
@@ -0,0 +1,4 @@
+variable "image_tag" {
+  description = "The image tag to use for the docker-cloud container"
+  default     = "latest"
+}
