257f989e57864333c553ce87b9c86fcae31de4c5
Berkeli Halmyradov <berkelih@gmail.com>
2022-11-18 11:48:25 +0000

    improved prober

--- buggy-app/cmd/chaos_monkey/main.go
+++ buggy-app/cmd/chaos_monkey/main.go
@@ -7,13 +7,14 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
-	"path/filepath"
 	"strings"
-	"sync"
+	"time"
 
 	"github.com/CodeYourFuture/immersive-go-course/buggy-app/util"
 	"github.com/XANi/loremipsum"
 	"github.com/jackc/pgx/v5"
+	"github.com/k0kubun/go-ansi"
+	"github.com/schollz/progressbar/v3"
 	"golang.org/x/sync/semaphore"
 )
 
@@ -22,7 +23,7 @@ const (
 	APP_URL  = "http://localhost:8090"
 )
 
-var commands = []string{"cache", "http"}
+var commands = []string{"notes", "http"}
 
 func contains(cmds []string, cmd string) bool {
 	for _, c := range cmds {
@@ -68,58 +69,56 @@ func main() {
 
 	// Get all users
 	users, err := GetAllUsers(conn)
-
 	if err != nil {
 		log.Fatalf("error getting users: %v", err)
 	}
-
+	log.Printf("Found %d users", len(users))
 	switch cmd {
-	case "cache":
-		//remove notes from users so we are only attacking the cache
-		clearNotes(conn, ctx)
-		err = MakeRequests(ctx, users)
-	case "http":
-		err = CreateNotes(conn, users[0], 1000)
+	case "notes":
+		log.Println("Adding notes to all users")
+		err = CreateNotes(conn, users, 1000)
 		if err != nil {
 			log.Fatalf("error creating notes: %v", err)
 		}
-		err = MakeRequests(ctx, []string{users[0], users[0], users[0], users[0]})
-	}
-
-	if err != nil {
-		log.Fatalf("error attacking server: %v", err)
+	case "http":
+		log.Println("Attacking the server with HTTP requests")
+		done := make(chan bool)
+		go HealthCheck(done)
+		MakeRequests(ctx, users, done)
+	default:
+		log.Fatalf("Invalid command, please provie what to attack (%s)", strings.Join(commands, ", "))
 	}
 }
 
-func clearNotes(conn *pgx.Conn, ctx context.Context) {
-	// migrate notes down
-	log.Println("Clearing notes table")
-	path := filepath.Join("migrations", "app", "000003_create_note_table.down.sql")
+// func clearNotes(conn *pgx.Conn, ctx context.Context) {
+// 	// migrate notes down
+// 	log.Println("Clearing notes table")
+// 	path := filepath.Join("migrations", "app", "000003_create_note_table.down.sql")
 
-	migrate := func(path string) {
-		c, ioErr := os.ReadFile(path)
-		if ioErr != nil {
-			log.Fatal(ioErr)
-		}
-		sql := string(c)
-		_, err := conn.Exec(ctx, sql)
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+// 	migrate := func(path string) {
+// 		c, ioErr := os.ReadFile(path)
+// 		if ioErr != nil {
+// 			log.Fatal(ioErr)
+// 		}
+// 		sql := string(c)
+// 		_, err := conn.Exec(ctx, sql)
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 	}
 
-	migrate(path)
+// 	migrate(path)
 
-	// migrate up
-	path = filepath.Join("migrations", "app", "000003_create_note_table.up.sql")
+// 	// migrate up
+// 	path = filepath.Join("migrations", "app", "000003_create_note_table.up.sql")
 
-	migrate(path)
+// 	migrate(path)
 
-	log.Println("Finished clearing notes")
-}
+// 	log.Println("Finished clearing notes")
+// }
 
 func GetAllUsers(conn *pgx.Conn) ([]string, error) {
-	rows, err := conn.Query(context.Background(), "SELECT id FROM public.user WHERE status='active'")
+	rows, err := conn.Query(context.Background(), "SELECT id FROM public.user WHERE status='active' LIMIT 100")
 	if err != nil {
 		return nil, err
 	}
@@ -137,43 +136,44 @@ func GetAllUsers(conn *pgx.Conn) ([]string, error) {
 	return users, nil
 }
 
-func MakeRequests(ctx context.Context, users []string) error {
-	// authenticate all users at the same time which will overload the cache
-	wg := &sync.WaitGroup{}
+func MakeRequests(ctx context.Context, users []string, done <-chan bool) {
+	// authenticate all users at the same time which will overload the server
 	sem := semaphore.NewWeighted(175)
-	done := make(chan bool)
 	client := &http.Client{
 		Transport: &http.Transport{
 			MaxConnsPerHost: 9999,
 		},
 	}
 	id := 0
+
+	bar := progressbar.NewOptions(-1,
+		progressbar.OptionSetDescription("Authenticating users..."),
+		progressbar.OptionSetWriter(ansi.NewAnsiStdout()),
+	)
+
 	for {
 		select {
-		case <-ctx.Done():
-			return ctx.Err()
 		case <-done:
 			log.Println("Successfully crashed server")
-			return nil
+			ctx.Done()
 		default:
 			ok := sem.TryAcquire(1)
 			if !ok {
 				continue
 			} else {
 				id++
+				bar.Add(1)
 				if id >= len(users) {
 					id = 0
 				}
-				wg.Add(1)
-				go AuthenticateUser(ctx, wg, sem, client, users[id], done)
+				go AuthenticateUser(ctx, sem, client, users[id])
 			}
 		}
 	}
 }
 
-func AuthenticateUser(ctx context.Context, wg *sync.WaitGroup, sem *semaphore.Weighted, client *http.Client, user string, done chan bool) {
+func AuthenticateUser(ctx context.Context, sem *semaphore.Weighted, client *http.Client, user string) {
 	defer sem.Release(1)
-	defer wg.Done()
 	req, err := http.NewRequest("GET", APP_URL+"/1/my/notes.json", nil)
 	if err != nil {
 		log.Println(err)
@@ -182,12 +182,23 @@ func AuthenticateUser(ctx context.Context, wg *sync.WaitGroup, sem *semaphore.We
 	_, err = client.Do(req)
 
 	if err != nil {
-		ctx.Done()
-		done <- true
+		log.Println(err)
 	}
 }
 
-func CreateNotes(conn *pgx.Conn, user string, n int) error {
+func HealthCheck(done chan bool) {
+	fmt.Println("Starting health check")
+	for {
+		time.Sleep(500 * time.Millisecond)
+		resp, err := http.Get(APP_URL + "/1/health")
+		if err != nil || resp.StatusCode != 200 {
+			log.Println(err)
+			done <- true
+		}
+	}
+}
+
+func CreateNotes(conn *pgx.Conn, users []string, n int) error {
 	// This function will make a large request to the server
 	// This will cause the server to crash
 	batchSize := 100
@@ -195,21 +206,22 @@ func CreateNotes(conn *pgx.Conn, user string, n int) error {
 	// add some hashtags
 	content := strings.Replace(loremIpsumGenerator.Paragraph(), " ", " #", 6)
 	// create 100 notes SQL statement
-	sql := "INSERT INTO public.note (owner, content) VALUES "
-	for i := 0; i < batchSize; i++ {
-		sql += fmt.Sprintf("('%s', '%s'),", user, content)
-	}
+	log.Println(content)
+	for _, user := range users {
+		sql := "INSERT INTO public.note (owner, content) VALUES "
+		for i := 0; i < batchSize; i++ {
+			sql += fmt.Sprintf("('%s', '%s'),", user, content)
+		}
 
-	// remove last comma
-	sql = sql[:len(sql)-1]
-
-	// execute SQL statement
-	for i := 0; i < n; i += batchSize {
-		_, err := conn.Exec(context.Background(), sql)
-		if err != nil {
-			return err
+		// remove last comma
+		sql = sql[:len(sql)-1]
+		// execute SQL statement
+		for i := 0; i < n; i += batchSize {
+			_, err := conn.Exec(context.Background(), sql)
+			if err != nil {
+				return err
+			}
 		}
 	}
-
 	return nil
 }
